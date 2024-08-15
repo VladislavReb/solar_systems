bcf812bb03d077f771d932178701199313c169b9
VladislavReb <104519014+VladislavReb@users.noreply.github.com>
2024-08-15 16:15:34 +0200

    add new project

---
+++ index.js
@@ -0,0 +1,19 @@
+$('#slider_1').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    nextArrow: '.arrow_2',
+    prevArrow: '.arrow',
+});
+
+$('#slider_2').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    nextArrow: '.arrow_4',
+    prevArrow: '.arrow_3',
+});
